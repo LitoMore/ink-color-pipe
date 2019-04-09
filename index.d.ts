186f7fd34b76ab2ebbafa6447160db78b657d284
LitoMore <litomore@gmail.com>
2019-04-09 23:23:41 +0800

    Add TypeScript definition

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export type ColorPipeProps = {
+	styles: string;
+}
+
+export default class ColorPipe extends React.Component<ColorPipeProps> {}
---
+++ index.test-d.tsx
@@ -0,0 +1,4 @@
+import * as React from 'react';
+import ColorPipe from '.';
+
+const ColorComponent = () => <ColorPipe styles="cyan.underline.bgWhite">ink-color-pipe</ColorPipe>;
